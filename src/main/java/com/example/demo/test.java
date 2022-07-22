cda3a075cad0d5f457d7c9a8d73ca792cf4c33b6
abhi5981 <abhi5981@gmail.com>
2022-07-22 21:11:36 +0530

    first commit

---
+++ src/main/java/com/example/demo/test.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, I am test !");
+        int n = 0;
+        n++;
+        System.out.println(n);
+
+    }
+
+}
